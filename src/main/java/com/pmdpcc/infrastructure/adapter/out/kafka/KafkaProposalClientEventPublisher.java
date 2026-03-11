a0866729fb12e02e6e520f9f8d0d4487a050d9cb
Bastian Prado <bastian.prado.v@gmail.com>
2026-03-11 14:36:27 -0700

    Publish message to kafka

--- src/main/java/com/pmdpcc/infrastructure/adapter/out/kafka/KafkaProposalClientEventPublisher.java
+++ src/main/java/com/pmdpcc/infrastructure/adapter/out/kafka/KafkaProposalClientEventPublisher.java
@@ -5,26 +5,30 @@ import com.pmdpcc.domain.model.ProposalClient;
 import com.pmdpcc.infrastructure.adapter.out.kafka.dto.ProposalEvent;
 import com.pmdpcc.infrastructure.adapter.out.kafka.mapper.ProposalEventMapper;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
-@AllArgsConstructor
 @Slf4j
+@RequiredArgsConstructor
 public class KafkaProposalClientEventPublisher implements ProposalClientEventPublisher {
 
     private final KafkaTemplate<String, ProposalEvent> kafkaTemplate;
     private final ProposalEventMapper mapper;
 
+    @Value("${spring.kafka.producer.topic}")
+    private String topicName;
+
     @Override
     public void publish(ProposalClient proposalClient) {
-        log.info("Publishing proposal to Kafka for client: {}", proposalClient.getClientName());
+        log.info("Publishing proposal to Kafka topic '{}' for client: {}", topicName, proposalClient.getClientName());
         ProposalEvent proposalEvent = mapper.toDTO(proposalClient);
 
-        // Publish to Kafka
-        // kafkaTemplate.send("output-topic", event);
+        kafkaTemplate.send(topicName, proposalEvent);
 
-        log.info("Proposal published to Kafka successfully for client: {}", proposalClient.getClientName());
+        log.info("Proposal published to Kafka topic '{}' successfully for client: {}", topicName, proposalClient.getClientName());
     }
 }
